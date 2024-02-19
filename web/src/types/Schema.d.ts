3f0d2371f838dfcb4a7d4a65ae544fa9973e0ba3
liuchenqi <374166002@qq.com>
2024-02-19 11:10:39 +0800

    格式化

--- web/src/types/Schema.d.ts
+++ web/src/types/Schema.d.ts
@@ -3,322 +3,324 @@
  * Do not make direct changes to the file.
  */
 
+
 export interface paths {
-  '/admin/sys/user/create': {
+  "/admin/sys/user/create": {
     /** 创建用户 */
-    post: operations['UserController_createUser']
-  }
-  '/admin/sys/login': {
+    post: operations["UserController_createUser"];
+  };
+  "/admin/sys/login": {
     /** 登录 */
-    post: operations['LoginController_login']
-  }
-  '/admin/sys/login/code': {
+    post: operations["LoginController_login"];
+  };
+  "/admin/sys/login/code": {
     /** 获取验证码 */
-    get: operations['LoginController_getCode']
-  }
-  '/admin/sys/menu/page': {
+    get: operations["LoginController_getCode"];
+  };
+  "/admin/sys/menu/page": {
     /** 分页菜单列表 */
-    post: operations['MenuController_getMenuPageList']
-  }
-  '/admin/sys/menu/list': {
+    post: operations["MenuController_getMenuPageList"];
+  };
+  "/admin/sys/menu/list": {
     /** 菜单列表 */
-    post: operations['MenuController_getMenuList']
-  }
-  '/admin/sys/menu/save': {
+    post: operations["MenuController_getMenuList"];
+  };
+  "/admin/sys/menu/save": {
     /** 保存菜单 */
-    post: operations['MenuController_saveMenu']
-  }
-  '/admin/sys/menu/id': {
+    post: operations["MenuController_saveMenu"];
+  };
+  "/admin/sys/menu/id": {
     /** id查询菜单详情 */
-    get: operations['MenuController_getMenuById']
-  }
-  '/admin/sys/menu/delete': {
+    get: operations["MenuController_getMenuById"];
+  };
+  "/admin/sys/menu/delete": {
     /** id删除菜单 */
-    post: operations['MenuController_deleteMenuById']
-  }
-  '/admin/sys/menu/routes': {
+    post: operations["MenuController_deleteMenuById"];
+  };
+  "/admin/sys/menu/routes": {
     /** 获取路由 */
-    get: operations['MenuController_getRoutes']
-  }
+    get: operations["MenuController_getRoutes"];
+  };
 }
 
-export type webhooks = Record<string, never>
+export type webhooks = Record<string, never>;
 
 export interface components {
   schemas: {
     CreateUserDto: {
-      createUserId?: string
+      createUserId?: string;
       /** @description 用户名 */
-      username: string
+      username: string;
       /** @description 密码 */
-      password: string
+      password: string;
       /** @description 姓名 */
-      realname: string
+      realname: string;
       /** @description 头像 */
-      avatar?: string
+      avatar?: string;
       /** @description 邮箱 */
-      email?: string
+      email?: string;
       /** @description 手机号 */
-      phone?: string
-    }
+      phone?: string;
+    };
     LoginDto: {
       /** @description 用户名 */
-      username: string
+      username: string;
       /** @description 密码 */
-      password: string
+      password: string;
       /** @description 验证码 */
-      code: string
+      code: string;
       /** @description 验证码id */
-      codeId: string
-    }
+      codeId: string;
+    };
     Token: {
       /** @description Token */
-      token: string
-    }
+      token: string;
+    };
     Code: {
       /** @description 验证码SVG */
-      img: string
+      img: string;
       /** @description 唯一id */
-      id: string
-    }
+      id: string;
+    };
     MenuPageListDto: {
       /**
        * @description 当前页码
        * @default 1
        */
-      currentPage?: number
+      currentPage?: number;
       /**
        * @description 页大小
        * @default 10
        */
-      pageSize?: number
+      pageSize?: number;
       /** @description 名称:类型为菜单时必填 */
-      name?: string
+      name?: string;
       /**
        * @description 是否启用:0-停用,1-启用
        * @default 1
        */
-      status?: number
-    }
-    PageResultDto: Record<string, never>
+      status?: number;
+    };
+    PageResultDto: Record<string, never>;
     MenuEntity: {
       /** Format: date-time */
-      createTime?: string
+      createTime?: string;
       /** Format: date-time */
-      updateTime?: string
-      createUserId?: string
-      updateUserId?: string
+      updateTime?: string;
+      createUserId?: string;
+      updateUserId?: string;
       /** @description 菜单id,新增时不需要传,更新时需要传 */
-      id?: string
+      id?: string;
       /** @description 父菜单id */
-      parentId?: string
+      parentId?: string;
       /** @description 名称:类型为菜单时必填 */
-      name?: string
+      name?: string;
       /** @description 路由页面时必填 */
-      path?: string
+      path?: string;
       /** @description 路由页面时必填 */
-      component?: string
+      component?: string;
       /** @description 菜单图标 */
-      icon?: string
+      icon?: string;
       /** @description 菜单类型:0-一级菜单,1-子菜单,2-权限 */
-      menuType: number
+      menuType: number;
       /** @description 类型为权限时必填 */
-      perms?: string[]
+      perms?: string[];
       /** @description 排序 */
-      sort: number
+      sort: number;
       /**
        * @description 是否缓存:0-不缓存,1-缓存
        * @default 1
        */
-      keepalive?: number
+      keepalive?: number;
       /**
        * @description 是否启用:0-停用,1-启用
        * @default 1
        */
-      status?: number
-    }
+      status?: number;
+    };
     MenuSaveDto: {
       /** @description 菜单列表 */
-      list: components['schemas']['MenuEntity'][]
-    }
-    DeleteResult: Record<string, never>
-  }
-  responses: never
-  parameters: never
-  requestBodies: never
-  headers: never
-  pathItems: never
+      list: components["schemas"]["MenuEntity"][];
+    };
+    DeleteResult: Record<string, never>;
+  };
+  responses: never;
+  parameters: never;
+  requestBodies: never;
+  headers: never;
+  pathItems: never;
 }
 
-export type $defs = Record<string, never>
+export type $defs = Record<string, never>;
 
-export type external = Record<string, never>
+export type external = Record<string, never>;
 
 export interface operations {
+
   /** 创建用户 */
   UserController_createUser: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     requestBody: {
       content: {
-        'multipart/form-data': components['schemas']['CreateUserDto']
-      }
-    }
+        "multipart/form-data": components["schemas"]["CreateUserDto"];
+      };
+    };
     responses: {
       201: {
-        content: never
-      }
-    }
-  }
+        content: never;
+      };
+    };
+  };
   /** 登录 */
   LoginController_login: {
     requestBody: {
       content: {
-        'application/json': components['schemas']['LoginDto']
-      }
-    }
+        "application/json": components["schemas"]["LoginDto"];
+      };
+    };
     responses: {
       /** @description 登录成功 */
       200: {
         content: {
-          'application/json': components['schemas']['Token']
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["Token"];
+        };
+      };
+    };
+  };
   /** 获取验证码 */
   LoginController_getCode: {
     responses: {
       /** @description 获取验证码成功 */
       200: {
         content: {
-          'application/json': components['schemas']['Code']
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["Code"];
+        };
+      };
+    };
+  };
   /** 分页菜单列表 */
   MenuController_getMenuPageList: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     requestBody: {
       content: {
-        'application/json': components['schemas']['MenuPageListDto']
-      }
-    }
+        "application/json": components["schemas"]["MenuPageListDto"];
+      };
+    };
     responses: {
       /** @description 分页菜单列表 */
       200: {
         content: {
-          'application/json': components['schemas']['PageResultDto']
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["PageResultDto"];
+        };
+      };
+    };
+  };
   /** 菜单列表 */
   MenuController_getMenuList: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     requestBody: {
       content: {
-        'application/json': components['schemas']['MenuPageListDto']
-      }
-    }
+        "application/json": components["schemas"]["MenuPageListDto"];
+      };
+    };
     responses: {
       /** @description 菜单列表 */
       200: {
         content: {
-          'application/json': components['schemas']['MenuEntity'][]
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["MenuEntity"][];
+        };
+      };
+    };
+  };
   /** 保存菜单 */
   MenuController_saveMenu: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     requestBody: {
       content: {
-        'application/json': components['schemas']['MenuSaveDto']
-      }
-    }
+        "application/json": components["schemas"]["MenuSaveDto"];
+      };
+    };
     responses: {
       /** @description 保存菜单 */
       200: {
         content: {
-          'application/json': components['schemas']['MenuEntity'][]
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["MenuEntity"][];
+        };
+      };
+    };
+  };
   /** id查询菜单详情 */
   MenuController_getMenuById: {
     parameters: {
       query: {
-        id: string
-      }
+        id: string;
+      };
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     responses: {
       /** @description id查询菜单详情 */
       200: {
         content: {
-          'application/json': components['schemas']['MenuEntity']
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["MenuEntity"];
+        };
+      };
+    };
+  };
   /** id删除菜单 */
   MenuController_deleteMenuById: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     responses: {
       /** @description id删除菜单 */
       200: {
         content: {
-          'application/json': components['schemas']['DeleteResult']
-        }
-      }
-    }
-  }
+          "application/json": components["schemas"]["DeleteResult"];
+        };
+      };
+    };
+  };
   /** 获取路由 */
   MenuController_getRoutes: {
     parameters: {
       header?: {
         /** @description Bearer token */
-        Authorization?: string
-      }
-    }
+        Authorization?: string;
+      };
+    };
     responses: {
       /** @description 获取路由 */
       200: {
         content: {
-          'application/json': string[]
-        }
-      }
-    }
-  }
+          "application/json": string[];
+        };
+      };
+    };
+  };
 }
