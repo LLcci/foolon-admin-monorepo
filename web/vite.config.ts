eac955cbf4d7c1e5fe4565b29abf4e2a8c10f195
liuchenqi <374166002@qq.com>
2024-08-12 16:15:29 +0800

    refactor: 取消px2vw

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,22 +8,6 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-import pxtovw from 'postcss-px-conversion'
-const loder_pxtovw = pxtovw({
-  viewportWidth: 375,
-  //@ts-ignore
-  viewportUnit: 'vw',
-  unitPrecision: 5,
-  //@ts-ignore
-  fontViewportUnit: 'vw',
-  allowMediaQuery: true,
-  enableLandscape: true,
-  selectorBlacklist: [new RegExp('.el-')],
-  //@ts-ignore
-  landscapeUnit: 'vw',
-  landscapeViewportWidth: 1920
-})
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -41,11 +25,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    postcss: {
-      plugins: [loder_pxtovw]
     }
   }
 })
