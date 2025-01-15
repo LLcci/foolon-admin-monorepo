4e2660c767d16a142db2f3b46dfb85e6727f7f51
liuchenqi <374166002@qq.com>
2025-01-15 14:59:03 +0800

    fix: 优化登录密码校验

--- api/src/admin/system/login/login.dto.ts
+++ api/src/admin/system/login/login.dto.ts
@@ -2,7 +2,12 @@ import { UserEntity } from '@/admin/system/user/user.entity'
 import { ApiProperty, PickType } from '@nestjs/swagger'
 import { IsNotEmpty, IsString } from 'class-validator'
 
-export class LoginDto extends PickType(UserEntity, ['username', 'password']) {
+export class LoginDto extends PickType(UserEntity, ['username']) {
+  @ApiProperty({ description: '密码' })
+  @IsString()
+  @IsNotEmpty()
+  password: string
+
   @ApiProperty({ description: '验证码' })
   @IsString()
   @IsNotEmpty()
